ea354c63f4a54d08363318525376ac77f6d793d9
KKXiaoKang <yangxiaokang@lejurobot.com>
2024-05-16 17:51:57 +0800

    feat:增加fps标识，判断当前的帧率

--- ONNX-YOLOv5-Instance-Segmentation/realsense_instance_segmentation.py
+++ ONNX-YOLOv5-Instance-Segmentation/realsense_instance_segmentation.py
@@ -1,15 +1,20 @@
+"""
+    在使用前请在yolov5文件夹对于
+    python export.py --weights yolov5s-seg.pt --include onnx
+"""
 import pyrealsense2 as rs
 import numpy as np
 import cv2
 import matplotlib
+import time
 
 from yoloseg import YOLOSeg
 
 # 配置深度和彩色流
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 
 # 启动管道
 pipeline.start(config)
@@ -21,6 +26,9 @@ cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
 
 try:
     while True:
+        
+        start_time = time.time()  # 记录推理开始时间
+
         # 等待获取一帧
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
@@ -43,6 +51,11 @@ try:
         combined_img = yoloseg.draw_masks(color_image)
         cv2.imshow("Detected Objects", combined_img)
         
+        end_time = time.time()  # 记录推理结束时间
+        inference_time = end_time - start_time  # 计算推理时间
+        fps = 1 // inference_time
+        print(f" FPS : {fps:.4f} HZ")
+
         # SHOW
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
